3bd656234c79ccc759e9378f6d50a3cc61c5669b
Stanley <stanleyyylau@gmail.com>
2016-10-12 15:15:00 +0800

    allow CORS

--- server.js
+++ server.js
@@ -21,7 +21,11 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static('public'));
 
-
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 app.post('/all_work', function(req, res){
   console.log('someone wants to get all your work details!!!');
